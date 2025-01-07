cffee60012466d4de398195b4149a9639afe2153
Ali Almasi <ali85almasi@gmail.com>
2025-01-07 17:49:36 +0330

    logo printed successfully

--- main.c
+++ main.c
@@ -1,17 +1,24 @@
 #include "funcs.h"
+#include "menu.h"
 #include <ncurses.h>
+#include <stdlib.h>
 
 int main() {
+    // initial commands :
     initscr();
-    
-    if (!has_colors()) {
-        endwin();
-        printf("Your terminal does not support colors.\n");
-        return 1;
-    }
     start_color();
+    cbreak();
+    keypad(stdscr, true);
+    noecho();
+    // color pairs :
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
+    init_pair(2, COLOR_BLUE, COLOR_WHITE);
+    // draw_logo(1);
     
-    
+    int choose = draw_menu();
+    attron(COLOR_PAIR(1));
+    printw("your choose is : %d", choose);
+    attroff(COLOR_PAIR(1));
     getch();
     endwin();
 }
--- menu.h
+++ menu.h
@@ -1,4 +1,10 @@
 #ifndef MENU_H
 #define MENU_H
+extern int draw_menu();
+extern void draw_logo(int y);
+#endif
 
-#endif // !MENU_H
+
+
+
+
